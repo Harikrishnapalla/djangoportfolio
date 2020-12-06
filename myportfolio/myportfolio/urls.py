1e218326eaa26d588031bfdd0acd389c838909eb
Harikrishna <harikrishna.py3@gmail.com>
2020-12-06 22:27:55 +0530

    created .po file

--- myportfolio/myportfolio/urls.py
+++ myportfolio/myportfolio/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('', views.home)
 ]
--- myportfolio/portfolio/views.py
+++ myportfolio/portfolio/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.utils.translation import gettext as _
 # Create your views here.
+
+def home(request):
+    return HttpResponse(_("test_world"))
